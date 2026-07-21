416a05d505695b2f2101dc85e757d75d6ae8f257
andruuragan <15252504@ukr.net>
2026-07-21 07:38:10 +0000

    Add Brevo API test

--- app/Services/BrevoMailService.php
+++ app/Services/BrevoMailService.php
@@ -2,41 +2,33 @@
 
 namespace App\Services;
 
-use Brevo\Client\Api\TransactionalEmailsApi;
-use Brevo\Client\Configuration;
-use Brevo\Client\Model\SendSmtpEmail;
-use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class BrevoMailService
 {
-    protected TransactionalEmailsApi $api;
-
-    public function __construct()
-    {
-        $config = Configuration::getDefaultConfiguration()
-            ->setApiKey('api-key', env('BREVO_API_KEY'));
-
-        $this->api = new TransactionalEmailsApi(
-            new Client(),
-            $config
-        );
-    }
-
     public function sendOrderMail($toEmail, $toName, $subject, $html)
     {
-        $email = new SendSmtpEmail([
+        return Http::withHeaders([
+            'accept' => 'application/json',
+            'api-key' => env('BREVO_API_KEY'),
+            'content-type' => 'application/json',
+        ])->post('https://api.brevo.com/v3/smtp/email', [
+
             'sender' => [
+                'name' => env('MAIL_FROM_NAME'),
                 'email' => env('MAIL_FROM_ADDRESS'),
-                'name'  => env('MAIL_FROM_NAME'),
             ],
-            'to' => [[
-                'email' => $toEmail,
-                'name'  => $toName,
-            ]],
+
+            'to' => [
+                [
+                    'email' => $toEmail,
+                    'name' => $toName,
+                ]
+            ],
+
             'subject' => $subject,
+
             'htmlContent' => $html,
         ]);
-
-        return $this->api->sendTransacEmail($email);
     }
 }
